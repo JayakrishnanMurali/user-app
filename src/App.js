6b978a93784ed71a4b73a0130fed2ef9ebb2d39c
Jayakrishnan M <Jayakrishnan1012@gmail.com>
2022-08-22 15:07:21 +0530

    initial set up

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div >
+      <h1>Users</h1>
+      {/* Filter Component */}
+
+      {/* Card component */}
+
+      {/* Pagination COmponent */}
+
+      {/* Create User Button */}
+    </div>
+  );
+}
+
+export default App;
