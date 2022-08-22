7fdd061408f77447cc88a53aeda5df7c90e3cd83
Jayakrishnan M <Jayakrishnan1012@gmail.com>
2022-08-22 16:20:35 +0530

    add user api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { useEffect, useState } from "react";
+import { getUsers } from "./api/user";
 import Filter from "./components/Filter/Filter";
 import UserCard from "./components/Usercard/UserCard";
 
 function App() {
+  const [users, setUsers] = useState();
+
+  async function getUserData() {
+    let data = await getUsers();
+    setUsers(data);
+  }
+
+  useEffect(() => {
+    if (!users) getUserData();
+  }, []);
+
   return (
     <div className="p-16 ">
       <Filter />
 
-      <div className="my-16">
-        <UserCard />
+      <div className="my-16 grid grid-cols-3">
+        {users.map((user) => (
+          <UserCard user={user} />
+        ))}
       </div>
 
       {/* Pagination COmponent */}
--- src/components/Usercard/UserCard.js
+++ src/components/Usercard/UserCard.js
@@ -3,24 +3,24 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import React from "react";
 
-const UserCard = () => {
+const UserCard = ({ user }) => {
   return (
-    <div className="w-2/6 cursor-pointer">
+    <div className="p-4 cursor-pointer">
       <Card>
         <CardContent className="flex relative gap-12 justify-start items-center">
           <div>
             <img
-              src="https://source.unsplash.com/random"
+              src={user.avatarUrl}
               className="w-52 h-60 object-cover mb-4"
               alt=""
             />
-            <h3 className="text-xl">Age: 32</h3>
+            <h3 className="text-xl">Age: {user.age}</h3>
           </div>
 
           <div>
-            <h2 className="text-3xl mb-8">Jayakrishnan M</h2>
-            <h3 className=" text-xl mb-4">Watching Netflix</h3>
-            <h3 className=" text-xl mb-4">27-09-2020</h3>
+            <h2 className="text-3xl mb-8">{user.name}</h2>
+            <h3 className=" text-xl mb-4">{user.statusMessage}</h3>
+            <h3 className=" text-xl mb-4">{user.createdAt}</h3>
           </div>
 
           <div className="absolute right-8 bottom-8">
