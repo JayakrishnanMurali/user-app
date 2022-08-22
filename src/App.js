135d0b57ab420736e269adf322119e20b02aef81
Jayakrishnan M <Jayakrishnan1012@gmail.com>
2022-08-22 17:00:31 +0530

    add pagination

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { getUsers } from "./api/user";
 import Filter from "./components/Filter/Filter";
+import Pagination from "./components/Pagination/Pagination";
 import UserCard from "./components/Usercard/UserCard";
+
+let PageSize = 5; // CHANGE LATER
 
 function App() {
   const [users, setUsers] = useState();
-
+  const [currentPage, setCurrentPage] = useState(1);
   async function getUserData() {
     let data = await getUsers();
     setUsers(data);
@@ -15,21 +18,34 @@ function App() {
     if (!users) getUserData();
   }, []);
 
-  return (
-    <div className="p-16 ">
-      <Filter />
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return users?.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage, users]);
 
-      <div className="my-16 grid grid-cols-3">
-        {users.map((user) => (
-          <UserCard user={user} />
-        ))}
+  if (!users) return <>Loading..</>;
+  else
+    return (
+      <div className="p-16 ">
+        <Filter />
+
+        <div className="my-16 grid grid-cols-3">
+          {currentTableData?.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </div>
+
+        <Pagination
+          currentPage={currentPage}
+          totalCount={users?.length}
+          pageSize={PageSize}
+          onPageChange={(page) => setCurrentPage(page)}
+        />
+
+        {/* Create User Button */}
       </div>
-
-      {/* Pagination COmponent */}
-
-      {/* Create User Button */}
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { usePagination, DOTS } from "../../hooks/CustomPagination";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+const Pagination = (props) => {
+  const {
+    onPageChange,
+    totalCount,
+    siblingCount = 1,
+    currentPage,
+    pageSize,
+  } = props;
+
+  const paginationRange = usePagination({
+    currentPage,
+    totalCount,
+    siblingCount,
+    pageSize,
+  });
+
+  if (currentPage === 0 || paginationRange.length < 2) {
+    return null;
+  }
+
+  const onNext = () => {
+    onPageChange(currentPage + 1);
+  };
+
+  const onPrevious = () => {
+    onPageChange(currentPage - 1);
+  };
+
+  let lastPage = paginationRange[paginationRange.length - 1];
+
+  return (
+    <ul className="flex max-w-5xl m-auto text-2xl justify-center">
+      <h4
+        onClick={onPrevious}
+        className="text-4xl cursor-pointer flex justify-center text-center "
+      >
+        &lt;
+      </h4>
+      <li onClick={onPrevious}>
+        <div className="arrow left" />
+      </li>
+      {paginationRange.map((pageNumber, ind) => {
+        if (pageNumber === DOTS) {
+          return (
+            <li key={ind} className="pagination-item dots">
+              &#8230;
+            </li>
+          );
+        }
+
+        return (
+          <li
+            className="bg-gray-400 mx-2 rounded-md px-4 py-2 cursor-pointer"
+            onClick={() => onPageChange(pageNumber)}
+          >
+            {pageNumber}
+          </li>
+        );
+      })}
+      <h4
+        onClick={onNext}
+        className="text-4xl cursor-pointer flex justify-center text-center "
+      >
+        &gt;
+      </h4>
+      <li onClick={onNext}>
+        <div className="arrow right" />
+      </li>
+    </ul>
+  );
+};
+
+export default Pagination;
